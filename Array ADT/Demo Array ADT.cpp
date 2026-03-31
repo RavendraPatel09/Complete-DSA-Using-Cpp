d3fb45e209c73f9fb0be99d5246185c01e59cbd2
patelsamerth9 <patelsamerth9@gmail.com>
2026-03-31 23:12:02 +0530

    Demo array using Abstract data type with cpp

--- Array ADT/Demo Array ADT.cpp
+++ Array ADT/Demo Array ADT.cpp
@@ -6,11 +6,32 @@ struct Array
     int size;
     int length;
 };
+void display(struct Array arr)
+{
+    int i;
+    printf("Elements are: \n");
+    for(i=0;i<arr.length;i++)
+    {
+        printf("%d ",arr.A[i]);
+    }
+}
 int main()
 {
-    struct Array arr={{2,3,4,5,6},10,5};
-    printf("Enter the Size of an Array: ");\
+    struct Array arr;
+    int i,j;
+    printf("Enter the Size of an Array: ");
     scanf("%d",&arr.size);
     arr.A = (int *)malloc(arr.size*sizeof(int));
     arr.length=0;
+    printf("Enter the number of elements: ");
+    scanf("%d",&arr.length);
+
+    printf("Enter the elements: \n");
+    for(i=0;i<arr.length;i++)
+    {
+        printf("Enter element %d: ",i+1);
+        scanf("%d",&arr.A[i]);
+    }
+    return 0;
+    display(arr);
 }
