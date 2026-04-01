b45dd803882fbea6fa2d71e097e3178cc3c05361
patelsamerth9 <patelsamerth9@gmail.com>
2026-04-02 02:20:48 +0530

    Add implementation for binary search algorithm and related files

--- Array ADT/Binary Search.cpp
+++ Array ADT/Binary Search.cpp
@@ -14,34 +14,33 @@ void display(struct Array arr)
     printf("%d ",arr.A[i]);
     
 }
+void swap(int *x,int *y)
+{
+    int temp;
+    temp=*x;
+    *x=*y;
+    *y=temp;
+}
 int BinarySearch(struct Array *arr,int key)
 {
-    int low=0;
-    int high=arr->length-1;
-    int mid;
-    while(low<=high)
+int l,mid,h;
+l=0;
+h=arr->length-1;
+while(l<=h)
     {
-        mid=(low+high)/2;
+        mid=(l+h)/2;
         if(key==arr->A[mid])
-        {swap(&arr->A[mid],&arr->A[mid-1]);
         return mid;
-        }
         else if(key<arr->A[mid])
-        high=mid-1;
+        h=mid-1;
         else
-        low=mid+1;
-    }
-    {
-        if(key==arr->A[i])
-        {swap(&arr->A[i],&arr->A[i-1]);
-        return i;
-        }
+        l=mid+1;
     }
     return -1;
 }
 int main()
 {struct Array arr={{2,3,4,5,6},10,5};
-printf("Index of element is %d\n",BinarySearch(&arr,3));
+printf("Index of element is %d\n",BinarySearch(&arr,7));
 display(arr);
 return 0;
 }
