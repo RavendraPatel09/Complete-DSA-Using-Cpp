2f8c72632049040533f601a18ff319b8a09bc19e
patelsamerth9 <patelsamerth9@gmail.com>
2026-04-01 17:51:55 +0530

    insert element at index 5

--- Array ADT/Inserting array.cpp
+++ Array ADT/Inserting array.cpp
@@ -19,9 +19,20 @@ void append(struct Array *arr,int x)
     if(arr->length<arr->size)
     arr->A[arr->length++]=x;
 }
+void insert(struct Array *arr,int index,int x)
+{
+    int i;
+    if(index>=0 && index<=arr->length)
+    {
+        for(i=arr->length;i>index;i--)
+        arr->A[i]=arr->A[i-1];
+        arr->A[index]=x;
+        arr->length++;
+    }
+}
 int main()
 {struct Array arr={{2,3,4,5,6},10,5};
-append(&arr,10);
+insert(&arr,5,10);
 display(arr);
 return 0;
 }
