3cf9880ea52ecdc8aee19e73ce287f66187d6785
patelsamerth9 <patelsamerth9@gmail.com>
2026-04-02 01:33:12 +0530

    Refactor LinearSearch function to use pointer and add swap functionality

--- Array ADT/Linear Search Arr.cpp
+++ Array ADT/Linear Search Arr.cpp
@@ -14,19 +14,28 @@ void display(struct Array arr)
     printf("%d ",arr.A[i]);
     
 }
-int LinearSearch(struct Array arr,int key)
+void swap(int *x,int *y)
+{
+    int temp;
+    temp=*x;
+    *x=*y;
+    *y=temp;
+}
+int LinearSearch(struct Array *arr,int key)
 {
     int i;
-    for(i=0;i<arr.length;i++)
+    for(i=0;i<arr->length;i++)
     {
-        if(key==arr.A[i])
+        if(key==arr->A[i])
+        {swap(&arr->A[i],&arr->A[i-1]);
         return i;
+        }
     }
     return -1;
 }
 int main()
 {struct Array arr={{2,3,4,5,6},10,5};
-printf("Index of element is %d\n",LinearSearch(arr,6));
+printf("Index of element is %d\n",LinearSearch(&arr,3));
 display(arr);
 return 0;
 }
