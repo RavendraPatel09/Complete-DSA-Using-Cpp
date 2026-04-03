f20b4699f6cdb037bdb69cea408b8657584a8d50
patelsamerth9 <patelsamerth9@gmail.com>
2026-04-04 00:44:08 +0530

    Add new file for reversing array implementation

---
+++ Array ADT/Reversing Array.cpp
@@ -0,0 +1,97 @@
+#include <stdio.h>
+#include <stdlib.h>
+struct Array
+{
+    int A[10];
+    int size;
+    int length;
+};
+void display(struct Array arr)
+{
+    int i;
+    printf("Elements are: \n");
+    for(i=0;i<arr.length;i++)
+    printf("%d ",arr.A[i]);
+    
+}
+void swap(int *x,int *y)
+{
+    int temp;
+    temp=*x;
+    *x=*y;
+    *y=temp;
+}
+int LinearSearch(struct Array *arr,int key)
+{
+    int i;
+    for(i=0;i<arr->length;i++)
+    {
+        if(key==arr->A[i])
+        {swap(&arr->A[i],&arr->A[i-1]);
+        return i;
+        }
+    }
+    return -1;
+}
+int Get(struct Array arr,int index)
+{
+    if(index>=0 && index<arr.length)
+    return arr.A[index];
+    return -1;
+}
+void Set(struct Array *arr,int index,int x)
+{
+    if(index>=0 && index<arr->length)
+    arr->A[index]=x;
+}
+int Max(struct Array arr)
+{
+    int max=arr.A[0];
+    for(int i=1;i<arr.length;i++)
+    {
+        if(arr.A[i]>max)
+        max=arr.A[i];
+    }
+    return max;
+}
+int Min(struct Array arr)
+{
+    int min=arr.A[0];
+    for(int i=1;i<arr.length;i++)
+    {
+        if(arr.A[i]<min)
+        min=arr.A[i];
+    }
+    return min;
+}
+int Sum(struct Array arr)
+{
+    int sum=0;
+    for(int i=0;i<arr.length;i++)
+    sum+=arr.A[i];
+    return sum;
+}
+float Avg(struct Array arr)
+{
+    return (float)Sum(arr)/arr.length;
+}
+void reverse(struct Array *arr)
+{
+    int *B;
+    int i,j;
+    B=(int *)malloc(arr->length*sizeof(int));
+    for(i=arr->length-1,j=0;i>=0;i--,j++)
+    {
+        B[j]=arr->A[i];
+    }
+    for(i=0;i<arr->length;i++)
+    {
+        arr->A[i]=B[i];
+    }
+    free(B);
+}
+
+int main()
+{struct Array arr={{2,3,4,5,6},10,5};
+return 0;
+}
