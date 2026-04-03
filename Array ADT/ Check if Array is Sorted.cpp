856847fcdda8b4be936914489d7c90fd4e6e5d29
patelsamerth9 <patelsamerth9@gmail.com>
2026-04-04 01:39:54 +0530

    Add how to check wheather array is sorted or not

--- Array ADT/ Check if Array is Sorted.cpp
+++ Array ADT/ Check if Array is Sorted.cpp
@@ -71,40 +71,18 @@ int Sum(struct Array arr)
     sum+=arr.A[i];
     return sum;
 }
-float Avg(struct Array arr)
-{
-    return (float)Sum(arr)/arr.length;
-}
-void reverse(struct Array *arr)
-{
-    int *B;
-    int i,j;
-    B=(int *)malloc(arr->length*sizeof(int));
-    for(i=arr->length-1,j=0;i>=0;i--,j++)
-        B[j]=arr->A[i];
-    for(i=0;i<arr->length;i++)
-        arr->A[i]=B[i];
-}
-void Reverse2(struct Array *arr)
-{
-    int i,j;
-    for(i=0,j=arr->length-1;i<j;i++,j--)
-    {
-        swap(&arr->A[i],&arr->A[j]);
-    }
-}
 void InsertSort(struct Array *arr,int x)
 {
     int i=arr->length-1;
-if(arr->length==arr->size)
-return;
-while(i>=0 && arr->A[i]>x)
-}
-{
-    arr->A[i+1]=arr->A[i];
-    i--;
-arr->A[i+1]=x;
-arr->length++;
+    if(arr->length==arr->size)
+    return;
+    while(i>=0 && arr->A[i]>x)
+    {
+        arr->A[i+1]=arr->A[i];
+        i--;
+    }
+    arr->A[i+1]=x;
+    arr->length++;
 }
 int main()
 {struct Array arr={{2,3,4,5,6},10,5};
