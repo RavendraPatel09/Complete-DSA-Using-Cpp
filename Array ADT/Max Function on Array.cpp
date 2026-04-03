94bc4b9e8fa3fa06a1f58b7ba9302235fde61162
patelsamerth9 <patelsamerth9@gmail.com>
2026-04-03 22:35:34 +0530

    Add Max and Set functions for Array ADT implementation

---
+++ Array ADT/Max Function on Array.cpp
@@ -0,0 +1,62 @@
+#include <stdio.h>
+#include <stdlib.h>
+struct Array
+{
+    int A[10];
+    int size;
+    int length;
+};
+void display(struct Array arr)
+{
+    int i;
+    printf("Elements are: \n");
+    for(i=0;i<arr.length;i++)
+    printf("%d ",arr.A[i]);
+    
+}
+void swap(int *x,int *y)
+{
+    int temp;
+    temp=*x;
+    *x=*y;
+    *y=temp;
+}
+int LinearSearch(struct Array *arr,int key)
+{
+    int i;
+    for(i=0;i<arr->length;i++)
+    {
+        if(key==arr->A[i])
+        {swap(&arr->A[i],&arr->A[i-1]);
+        return i;
+        }
+    }
+    return -1;
+}
+int Get(struct Array arr,int index)
+{
+    if(index>=0 && index<arr.length)
+    return arr.A[index];
+    return -1;
+}
+void Set(struct Array *arr,int index,int x)
+{
+    if(index>=0 && index<arr->length)
+    arr->A[index]=x;
+}
+int Max(struct Array arr)
+{
+    int max=arr.A[0];
+    for(int i=1;i<arr.length;i++)
+    {
+        if(arr.A[i]>max)
+        max=arr.A[i];
+    }
+    return max;
+}
+int main()
+{struct Array arr={{2,3,4,5,6},10,5};
+
+display(arr);
+return 0;
+}
