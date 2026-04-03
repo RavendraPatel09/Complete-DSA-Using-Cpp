ea27b08ed7a18b839b27f179fb8ba3f643faa9c1
patelsamerth9 <patelsamerth9@gmail.com>
2026-04-04 00:49:42 +0530

    Refactor reverse function and add Reverse2 implementation for array reversal

--- Array ADT/Reversing Array.cpp
+++ Array ADT/Reversing Array.cpp
@@ -81,19 +81,22 @@ void reverse(struct Array *arr)
     int i,j;
     B=(int *)malloc(arr->length*sizeof(int));
     for(i=arr->length-1,j=0;i>=0;i--,j++)
-    {
         B[j]=arr->A[i];
-    }
     for(i=0;i<arr->length;i++)
-    {
         arr->A[i]=B[i];
+}
+void Reverse2(struct Array *arr)
+{
+    int i,j;
+    for(i=0,j=arr->length-1;i<j;i++,j--)
+    {
+        swap(&arr->A[i],&arr->A[j]);
     }
-    free(B);
 }
 
 int main()
 {struct Array arr={{2,3,4,5,6},10,5};
-reverse(&arr);
+Reverse2(&arr);
 display(arr); 
 return 0;
 }
