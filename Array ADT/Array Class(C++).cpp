1bbc8821f1b14dfd368d0130d440278f660d387c
patelsamerth9 <patelsamerth9@gmail.com>
2026-04-06 20:18:39 +0530

    Array Class C++

---
+++ Array ADT/Array Class(C++).cpp
@@ -0,0 +1,64 @@
+#include<iostream>
+using namespace std;
+class Array {
+private:
+    int *A;
+    int size;
+    int length;
+    public:
+    Array()
+    {
+        size = 10;
+        A = new int[10];
+        length = 0;
+        A = new int[size];
+
+    }
+    Array(int sz)
+    {
+        size = sz;
+        length = 0;
+        A = new int[sz];
+    }
+    ~Array()
+    {
+        delete []A;
+    }
+    void Display();
+    void Insert(int index, int x);
+    int Delete(int index);
+
+};
+void Array::Display()
+{
+    cout<<"Elements are: "<<endl;
+    for(int i=0; i<length; i++)
+        cout<<A[i]<<" ";
+    cout<<endl;
+}
+void Array::Insert(int index, int x)
+{
+    if(index>=0 && index<=length)
+    {
+        for(int i=length; i>index; i--)
+            A[i] = A[i-1];
+        A[index] = x;
+        length++;
+    }
+}
+int Array::Delete(int index)
+{    int x = 0;
+    if(index>=0 && index<length)
+    {
+        x = A[index];
+        for(int i=index; i<length-1; i++)
+            A[i] = A[i+1];
+        length--;
+    }
+    return x;
+}
+int main(int argc, char const *argv[])
+{
+
+    return 0;
+}
