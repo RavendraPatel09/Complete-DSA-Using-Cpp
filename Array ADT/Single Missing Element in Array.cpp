655d3b7856c00582f671cdf8e5661c96a9c13048
patelsamerth9 <patelsamerth9@gmail.com>
2026-04-06 23:31:57 +0530

    Add how can we search easily for our element in the array which was missing out there

--- Array ADT/Single Missing Element in Array.cpp
+++ Array ADT/Single Missing Element in Array.cpp
@@ -2,7 +2,7 @@
 using namespace std;
 int main()
 {
-    int arr[] = {1,2,3,4,5,6,7,8,9};
+    int arr[] = {1,2,3,5,6,7,8,9};
     int n = sizeof(arr)/sizeof(arr[0]);
     int total = (n+1)*(n+2)/2;
     int sum = 0;
