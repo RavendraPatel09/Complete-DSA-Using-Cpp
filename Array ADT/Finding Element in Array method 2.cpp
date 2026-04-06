587f4722e089830154610c25bdb722f7d1e9de67
patelsamerth9 <patelsamerth9@gmail.com>
2026-04-06 23:46:52 +0530

    Find any element method 2

---
+++ Array ADT/Finding Element in Array method 2.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int arr[] = {3,2,5,6,8,9,1,7,4};
+    int n = sizeof(arr)/sizeof(arr[0]);
+    int l = 1, h = n;
+    {
+    H[A[i]]++;
+        if(arr[i] - i != diff)
+    }
+    for(i=l ;i<=h ; i++)
+    {
+        if(H[i] == 0)
+        {
+            cout << "Missing element is: " << i << endl;
+        }
+    }
+    return 0;
+}
