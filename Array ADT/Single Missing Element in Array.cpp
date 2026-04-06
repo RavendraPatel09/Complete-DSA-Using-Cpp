0af7357b80c65b44e455fd65d94d11204bff296e
patelsamerth9 <patelsamerth9@gmail.com>
2026-04-06 23:30:34 +0530

    Correct the code and make it run smooth

--- Array ADT/Single Missing Element in Array.cpp
+++ Array ADT/Single Missing Element in Array.cpp
@@ -2,12 +2,12 @@
 using namespace std;
 int main()
 {
-    int arr[] = {1,2,3,4,5,6,7,8,9,10};
+    int arr[] = {1,2,3,4,5,6,7,8,9};
     int n = sizeof(arr)/sizeof(arr[0]);
-    int total_sum = (n+1)*(n+2)/2;
+    int total = (n+1)*(n+2)/2;
     int sum = 0;
     for(int i=0; i<n; i++)
-    if(arr[i]-i!= diff)
         sum += arr[i];
-    cout<<"Missing element is: "<<total_sum - sum<<endl;
+    cout<<"Missing element is: "<<total-sum<<endl;
+    return 0;
 }
