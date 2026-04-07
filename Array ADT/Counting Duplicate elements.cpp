d03ddd3dadecbee68e4775e6b3a7c1c16d6f99db
patelsamerth9 <patelsamerth9@gmail.com>
2026-04-08 00:44:20 +0530

    Counting number of duplicate elements in the given array

--- Array ADT/Counting Duplicate elements.cpp
+++ Array ADT/Counting Duplicate elements.cpp
@@ -1,6 +1,5 @@
 #include<iostream>
 using namespace std;
-
 int main()
 {
     int arr[] = {1,2,2,4,5,6,6,7,7,8,9};
@@ -8,7 +7,7 @@ int main()
     {
         if(arr[i] == arr[i+1])
         {
-            cout<<arr[i]<<" ";
+            cout<<arr[i]<<" is repeated "<<j-i<<" times"<<endl;
         }
         j = i+1;
         while(arr[i] == arr[j])
@@ -16,4 +15,4 @@ int main()
         }        i = j-1;
     }
     return 0;
-}
+};
