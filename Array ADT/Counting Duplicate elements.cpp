df8a7ae90bf62994d3a8ead235302cad26c6039e
patelsamerth9 <patelsamerth9@gmail.com>
2026-04-08 00:45:42 +0530

    find how many time and which which elements is repeated

--- Array ADT/Counting Duplicate elements.cpp
+++ Array ADT/Counting Duplicate elements.cpp
@@ -3,16 +3,19 @@ using namespace std;
 int main()
 {
     int arr[] = {1,2,2,4,5,6,6,7,7,8,9};
-    for(i=0 ; i<n-1 ; i++)
+    int n = sizeof(arr)/sizeof(arr[0]);
+    for(int i = 0; i < n-1; i++)
     {
         if(arr[i] == arr[i+1])
         {
-            cout<<arr[i]<<" is repeated "<<j-i<<" times"<<endl;
+            int j = i + 1;
+            while(j < n && arr[i] == arr[j])
+            {
+                j++;
+            }
+            cout << arr[i] << " is repeated " << j - i << " times" << endl;
+            i = j - 1;
         }
-        j = i+1;
-        while(arr[i] == arr[j])
-        {            j++;
-        }        i = j-1;
     }
     return 0;
-};
+}
