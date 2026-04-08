4c19b7c997e270dbbd14aca907edc68a48f522f0
patelsamerth9 <patelsamerth9@gmail.com>
2026-04-08 23:41:25 +0530

    we get -1 as min and 15 as max in our output

--- Array ADT/Finding Max and Min in single Scan.cpp
+++ Array ADT/Finding Max and Min in single Scan.cpp
@@ -2,17 +2,19 @@
 using namespace std;
 int main()
 {
-    int n;
-    int arr[]={1,2,3,4,5,10,-1,15}
-    min =A[0];
-    max =A[0];
-    for(int i=1;i<n;i++)
+    int arr[]={1,2,3,4,5,10,-1,15};
+    int min = arr[0];
+    int max = arr[0];
+    for(int i=1;i<8;i++)
     {
         if(arr[i]>max)
             max=arr[i];
         else if(arr[i]<min)
             min=arr[i];
-    }   
+    } 
+    cout<<"Max is "<<max<<endl;
+    cout<<"Min is "<<min<<endl;
+    return 0;
 }
 // Best case is when it in in decresing order 
 //worst case is when it is in increasing order
