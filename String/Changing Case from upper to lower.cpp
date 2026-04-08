00f2968de0eca6629b8645aedf3bcfe8318321ba
patelsamerth9 <patelsamerth9@gmail.com>
2026-04-09 01:21:17 +0530

    changing case from upper case to lower case for the given following string

---
+++ String/Changing Case from upper to lower.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    char S[]="WELCOME";
+    int i;
+    for(i=0;S[i]!='\0';i++)
+    {
+    A[i]=S[i]+32;
+
+    }
+    cout<<"String in lower case is "<<A<<endl;
+    return 0;
+}
+// Time complexity is O(n) where n is the length of the string
