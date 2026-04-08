8025f8ff3c485b83559f1a7c771188d7e122101a
patelsamerth9 <patelsamerth9@gmail.com>
2026-04-08 22:44:39 +0530

    Find repeated element in array using hashing

---
+++ Array ADT/Finding Duplicate using Hashing.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int n;
+    int arr[] = {3,2,5,6,8,9,1,7,4};
+    int l = 1, h = 12;
+    int H[100] = {0};
+    // Mark elements in hash table
+    for(int i = 0; i < n; i++)
+    {
+        H[arr[i]]++;
+    }
+    for(int i = l; i <= h; i++)
+    {
+        if(H[i] == 0)
+        {
+            cout << "Missing element is: " << i << endl;
+        }
+        
+    }
+    return 0;
+}
