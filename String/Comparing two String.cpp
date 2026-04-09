80200a005bdec78a6f9a99c3f0829215b9fff8ac
patelsamerth9 <patelsamerth9@gmail.com>
2026-04-09 13:17:10 +0530

    Comparing Two Different String while they are same or not

---
+++ String/Comparing two String.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    char A[]="Painter"
+    char B[]="Painting"
+    int i,j;
+    for(i=0; j=0; A[i] != '\0' && B[j] != '\0'; i++, j++)
+
+    {    if(A[i] != B[i])
+    break;
+    }
+    if(A[i] == B[j])
+    cout << "Both are same";
+    else if(A[i] < B[j])
+    cout << "A is smaller than B";
+    else
+    cout << "A is greater than B";
+    return 0;
+}
