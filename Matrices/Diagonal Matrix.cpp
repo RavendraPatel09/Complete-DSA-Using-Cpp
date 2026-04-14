53be42eb644aaf8e0eaa9d22f499cc1ec9dcff00
patelsamerth9 <patelsamerth9@gmail.com>
2026-04-15 01:22:55 +0530

    We got our matrix as output in the form of diagonal matrix

--- Matrices/Diagonal Matrix.cpp
+++ Matrices/Diagonal Matrix.cpp
@@ -8,34 +8,37 @@ struct matrix
 void set(struct matrix *m, int i, int j, int x)
 {
     if (i == j)
-    m->A[i - 1] = x;
+        m->A[i - 1] = x;
 }
 int get(struct matrix m, int i, int j)
 {
     if (i == j)
-    return m.A[i-1];
+        return m.A[i - 1];
     else
-    return 0;
+        return 0;
 }
 void display(struct matrix m)
-{    for (int i = 1; i <= m.n; i++)
-    {        for (int j = 0; j <= m.n; j++)
-        {
-            {
-                if (i == j)
-                cout << m.A[i - 1] << " ";
-                else
-                cout << "0 ";
-            }
-
-int main ()
 {
-struct matrix m;
-m.n = 4;
-set(&m, 1, 1, 5);
-set(&m, 2, 2, 8);
-set(&m, 3, 3, 9);
-set(&m, 4, 4, 6);
-display(m);
-    Return 0;
-}}}};
+    for (int i = 1; i <= m.n; i++)
+    {
+        for (int j = 1; j <= m.n; j++)
+        {
+            if (i == j)
+                cout << m.A[i - 1] << " ";
+            else
+                cout << "0 ";
+        }
+        cout << endl;
+    }
+}
+int main()
+{
+    struct matrix m;
+    m.n = 4;
+    set(&m, 1, 1, 5);
+    set(&m, 2, 2, 8);
+    set(&m, 3, 3, 9);
+    set(&m, 4, 4, 6);
+    display(m);
+    return 0;
+}
