2289a6872603d3999435d907ea4342100a1569a4
patelsamerth9 <patelsamerth9@gmail.com>
2026-04-19 23:11:34 +0530

    Addition of two different polynomial using C language

---
+++ Polynomial Representation/Addition of polynomial.cpp
@@ -0,0 +1,75 @@
+#include<stdio.h>
+#include<stdlib.h>
+struct Term
+{
+    int coeff;
+    int exp;
+};
+struct Poly
+{
+    int n;
+    struct Term *terms;
+};
+void create(struct Poly *p)
+{
+    int i;
+    printf("number of terms?");
+    scanf("%d",&p->n);
+    p->terms=(struct Term*)malloc(p->n*sizeof(struct Term));
+    printf("enter terms in decreasing order of exponent\n");
+    for(i=0;i<p->n;i++)
+    {
+        scanf("%d%d",&p->terms[i].coeff,&p->terms[i].exp);
+    }
+};
+void display(struct Poly p)
+{
+    int i;
+    for(i=0;i<p.n;i++)
+    {
+        printf("%dx^%d+",p.terms[i].coeff,p.terms[i].exp);
+    }
+    printf("\n");
+};
+struct Poly add(struct Poly p1,struct Poly p2)
+{
+    struct Poly sum;
+    sum.terms=(struct Term*)malloc((p1.n+p2.n)*sizeof(struct Term));
+    int i=0,j=0,k=0;
+    while(i<p1.n && j<p2.n)
+    {
+        if(p1.terms[i].exp>p2.terms[j].exp)
+        {
+            sum.terms[k++]=p1.terms[i++];
+        }
+        else if(p1.terms[i].exp<p2.terms[j].exp)
+        {
+            sum.terms[k++]=p2.terms[j++];
+        }
+        else
+        {
+            sum.terms[k]=p1.terms[i];
+            sum.terms[k++].coeff=p1.terms[i++].coeff+p2.terms[j++].coeff;
+        }
+    }
+    for(;i<p1.n;i++)
+    {
+        sum.terms[k++]=p1.terms[i];
+    }
+    for(;j<p2.n;j++)
+    {
+        sum.terms[k++]=p2.terms[j];
+    }
+    sum.n=k;
+    return sum;
+};
+int main()
+{    struct Poly p1,p2,sum;
+    create(&p1);
+    create(&p2);
+    display(p1);
+    display(p2);
+    sum=add(p1,p2);
+    display(sum);
+    return 0;
+}
