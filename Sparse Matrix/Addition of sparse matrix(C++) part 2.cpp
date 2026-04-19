0b85b3889a881c9a8d828145f52d476360dbf33a
patelsamerth9 <patelsamerth9@gmail.com>
2026-04-19 14:48:19 +0530

    ADD addition of sparse matrix using C++ method 2

--- Sparse Matrix/Addition of sparse matrix(C++) part 2.cpp
+++ Sparse Matrix/Addition of sparse matrix(C++) part 2.cpp
@@ -22,39 +22,84 @@ public:
         this->num=num;
         ele=new Element[this->num];
     }
+    sparse()
+    {
+        m=n=num=0;
+        ele=NULL;
+    }
     ~sparse()
     {
         delete [] ele;
     }
+    sparse operator+(sparse &s);
     friend istream & operator>>(istream &is,sparse &s);
     friend ostream & operator<<(ostream &os,sparse &s);
 };
-    istream & operator>>(istream &is,sparse &s)
+sparse sparse::operator+(sparse &s)
+{
+    int i,j,k;
+    if(m!=s.m || n!=s.n)
+        return sparse();
+    sparse sum(m,n,num+s.num);
+    i=j=k=0;
+    while(i<num && j<s.num)
     {
-        cout<<"Enter non-zero elements (i j x): ";
-        for(int i=0;i<s.num;i++)
-            cin>>s.ele[i].i>>s.ele[i].j>>s.ele[i].x;
-            return is;
-    }
-   ostream & operator<<(ostream &os,sparse &s)
-    {
-        int k=0;
-        for(int i=0;i<s.m;i++)
+        if(ele[i].i < s.ele[j].i)
+            sum.ele[k++] = ele[i++];
+        else if(ele[i].i > s.ele[j].i)
+            sum.ele[k++] = s.ele[j++];
+        else
         {
-            for(int j=0;j<s.n;j++)
+            if(ele[i].j < s.ele[j].j)
+                sum.ele[k++] = ele[i++];
+            else if(ele[i].j > s.ele[j].j)
+                sum.ele[k++] = s.ele[j++];
+            else
             {
-                if(k<s.num && s.ele[k].i==i && s.ele[k].j==j)
-                    cout<<s.ele[k++].x<<" ";
-                else
-                    cout<<"0 ";
+                sum.ele[k] = ele[i];
+                sum.ele[k++].x = ele[i++].x + s.ele[j++].x;
             }
-            cout<<endl; 
-        } 
-        return os;
+        }
     }
+    for(;i<num;i++)
+        sum.ele[k++] = ele[i];
+    for(;j<s.num;j++)
+        sum.ele[k++] = s.ele[j];
+    sum.num = k;
+    return sum;
+}
+istream & operator>>(istream &is,sparse &s)
+{
+    cout<<"Enter non-zero elements (i j x): ";
+    for(int i=0;i<s.num;i++)
+        is>>s.ele[i].i>>s.ele[i].j>>s.ele[i].x;
+    return is;
+}
+ostream & operator<<(ostream &os,sparse &s)
+{
+    int k=0;
+    for(int i=0;i<s.m;i++)
+    {
+        for(int j=0;j<s.n;j++)
+        {
+            if(k<s.num && s.ele[k].i==i && s.ele[k].j==j)
+                os<<s.ele[k++].x<<" ";
+            else
+                os<<"0 ";
+        }
+        os<<endl;
+    }
+    return os;
+}
 int main()
 {
     sparse s1(5,5,5);
+    sparse s2(5,5,5);
     cin>>s1;
-    cout<<s1;
+    cin>>s2;
+    sparse sum = s1 + s2;
+    cout<<"First matrix"<<endl<<s1;
+    cout<<"Second matrix"<<endl<<s2;
+    cout<<"Sum matrix"<<endl<<sum;
+    return 0;
 }
