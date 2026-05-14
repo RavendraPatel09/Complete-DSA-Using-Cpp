4b762abb6d0c0f462274d526441911d50c2b1795
patelsamerth9 <patelsamerth9@gmail.com>
2026-05-14 23:18:56 +0530

    Fix formatting by removing unnecessary whitespace in Finding Duplicates In array.cpp

--- Array ADT/Finding Duplicates In array.cpp
+++ Array ADT/Finding Duplicates In array.cpp
@@ -1,14 +1,11 @@
 #include<iostream>
 using namespace std;
-
 int main()
 {
     int arr[] = {1,2,2,4,5,6,6,7,7,8,9};
     int last_duplicate = -1;
     int n = sizeof(arr)/sizeof(arr[0]);
-
     cout << "Duplicate elements are: ";
-
     for(int i = 0; i < n-1; i++)
     {
         if(arr[i] == arr[i+1] && arr[i] != last_duplicate)
@@ -17,6 +14,5 @@ int main()
             last_duplicate = arr[i];
         }
     }
-
     return 0;
 }
